51eae5a03d1d573fbb746082f42b18082a49524f
300LiterPropofol <46748408+300LiterPropofol@users.noreply.github.com>
2019-12-17 20:29:16 +0100

    Delete delaySteerGyro.m

--- delaySteerGyro.m
+++
@@ -1,7 +0,0 @@
-function value = delaySteerGyro(time, st_rate, gyro_x, segment_start, segment_end)
-    if segment_start < segment_end
-        value = finddelay(gyro_x(segment_start:segment_end), st_rate(segment_start:segment_end)) * mean(diff(time(segment_start:segment_end)));
-    else
-        value = NaN;
-    end
-end
